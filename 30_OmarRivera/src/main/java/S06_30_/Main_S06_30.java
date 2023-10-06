9980d551e9a0eb79ccd1f4baa99f14343bc5d6b7
OmarRiv. <omar.rivera@vallegrande.edu.pe>
2023-10-06 15:02:46 -0500

    Agregando cambios s10_30

---
+++ 30_OmarRivera/src/main/java/S06_30_/Main_S06_30.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S06_30_;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main_S06_30 {
+    
+}
